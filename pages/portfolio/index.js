2babb452da71fd3720b95ac37a30d7e7c31e38b8
Morgan Segura <morgan@jointhebibleproject.com>
2021-05-21 20:46:01 -0700

    Implements SWR fetch caching

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,8 +1,11 @@
-import { useGetData } from '@/actions'
+import useSWR from 'swr'
+// import { useGetData } from '@/actions'
 import PortfolioLanding from '@components/portfolio/PortfolioLanding'
 
+const fetcher = url => fetch(url).then(res => res.json())
+
 const PortfolioHome = () => {
-	const { data, error, loading } = useGetData('/api/v1/posts')
+	const { data, error, loading } = useSWR('/api/v1/posts', fetcher)
 
 	return <PortfolioLanding loading={loading} error={error} posts={data} />
 }
